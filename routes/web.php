aa581d303d233e9d9d6a2f45b3c04b4e1b76f8c1
skid9000 <skid@tuto-craft.com>
2019-05-26 19:06:43 +0200

    Add Auth

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', function () {
+    return view('admin');
+})->name('admin')->middleware('auth:web');
